0800dc81dd2f3d6c667f76ef5dcab3d0d209eed6
moa234 <moaaztarek@hotmail.com>
2021-12-31 00:19:43 +0200

    input dice value

--- InputDiceValue.cpp
+++ InputDiceValue.cpp
@@ -1,1 +1,31 @@
 #include "InputDiceValue.h"
+# include "Grid.h"
+#include "Player.h"
+
+void InputDiceValue::ReadActionParameters()
+{
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+	pOut->PrintMessage("Enter the dice value:");
+
+	dicevalue = pIn->GetInteger(pOut); // read dice number from the user
+
+	if (dicevalue > 6 || dicevalue < 0)
+	{
+		dicevalue = 0;
+		pGrid->PrintErrorMessage("Invalid dice value! click to continue ...");
+	}
+}
+
+void InputDiceValue::Execute()
+{
+	Grid* pGrid = pManager->GetGrid();
+	pGrid->GetCurrentPlayer()->Move(pGrid, dicevalue);
+	pGrid->AdvanceCurrentPlayer();
+	
+}
+
+InputDiceValue::~InputDiceValue()
+{
+}
--- InputDiceValue.h
+++ InputDiceValue.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "Action.h"
-class InputDiceValue :
-    public Action
+class InputDiceValue : public Action
 {
+	int dicevalue;
+public:
+
+	virtual void ReadActionParameters();
+
+	virtual void Execute();
+
+	virtual ~InputDiceValue();
 };
 
