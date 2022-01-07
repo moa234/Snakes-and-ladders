dc80a07b34744bff3d282e81d17a7034f8ed3162
Hamdy <abdelrahmanhamdy.ha@gmail.com>
2022-01-07 22:56:26 +0200

    dice comments

--- RollDiceAction.cpp
+++ RollDiceAction.cpp
@@ -25,24 +25,32 @@ void RollDiceAction::Execute()
 	}
 
 	Player* currentplayer = pManager->GetGrid()->GetCurrentPlayer();
+
+	//Checking if the player is on card 4 or 8 ;so his dice roll is cancelled
 	if (currentplayer->GetDoNotPlay() > 0)
 	{
-		
+		//decreasing DoNotPlay
 		currentplayer->SetDoNotPlay((currentplayer->GetDoNotPlay()) - 1);
+
+		//calling move function to increase turnCount  and setting dice roll to zero.
 		currentplayer->Move(pManager->GetGrid(), 0);
 
 		pGrid->AdvanceCurrentPlayer();
 		return;
 	}
-
+	
+	//if DoNotPlay=0,the player will play normally.
 	srand((int)time(NULL));
 	int diceNumber = 1 + rand() % 6; 
 	currentplayer->SetRolledDiceNum(diceNumber);
 	currentplayer->Move(pManager->GetGrid(), diceNumber);
 
 	if (currentplayer->GetDoNotPlay() ==-1)
-	{
+	{	
 		currentplayer->SetDoNotPlay(0);
+		
+		//the player will return;thus the CurrentPlayer will not be advanced
+		//(he will roll the dice again)
 		return;
 	}
 
