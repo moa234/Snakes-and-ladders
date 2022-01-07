4f46ee835a3621f3c1cec73f19593f1a91b4b6be
moa234 <moaaztarek@hotmail.com>
2022-01-07 23:59:26 +0200

    card 7 comments

--- CardSeven.h
+++ CardSeven.h
@@ -2,16 +2,16 @@
 #include "Card.h"
 class CardSeven : public Card
 {
-	static const CellPosition firstCell;
+	static const CellPosition firstCell; // constant cellposition to first cell of grid
 public:
-	CardSeven(const CellPosition& pos);
+	CardSeven(const CellPosition& pos); // constructor
 
-	virtual void ReadCardParameters(Grid* pGrid);
-	virtual Card* PasteCard();
-	virtual Card* CopyCard();
-	virtual void Apply(Grid* pGrid, Player* pPlayer);
-	virtual void Save(ofstream& OutFile, Object_Type obj);
+	virtual void ReadCardParameters(Grid* pGrid); // virtual function wih empty implementation
+	virtual Card* PasteCard(); // function to create a new object to be added to grid when pasted
+	virtual Card* CopyCard(); // function to create a neew object to be added to clipboard when copied
+	virtual void Apply(Grid* pGrid, Player* pPlayer); //applies the card action to reset next player
+	virtual void Save(ofstream& OutFile, Object_Type obj); // save card to file
 
-	virtual ~CardSeven();
+	virtual ~CardSeven(); // virtual destructor
 };
 
