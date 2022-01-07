42a2da9295c47755e49fc81c3764787c6f65c74c
Hamdy <abdelrahmanhamdy.ha@gmail.com>
2022-01-08 00:13:08 +0200

    NEW KAME GOMMENTS

--- NewGame.h
+++ NewGame.h
@@ -3,8 +3,8 @@
 class NewGame: public Action
 {
 public:
-	NewGame(ApplicationManager* pApp);
-	virtual void Execute() ;  
+	NewGame(ApplicationManager* pApp);				//constructor
+	virtual void Execute() ;						//Restarts players' positions, wallets,turn returns to player 0
 	virtual void ReadActionParameters();
-	virtual ~NewGame();  
+	virtual ~NewGame();								//destructor
 };
