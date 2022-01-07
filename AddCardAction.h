b64bda58fb5c2df307f8f33f81d6f90d3b124b5d
Salah Mohamed <Salahabotaleb@outlook.com>
2022-01-07 23:29:37 +0200

    salah

--- AddCardAction.h
+++ AddCardAction.h
@@ -7,7 +7,7 @@ class AddCardAction : public Action
 	// [Action Parameters]
 	int cardNumber;            // 1- the card number
 	CellPosition cardPosition; // 2- cell position of the card
-	bool Can_Add;  //This is a boolean indecates the avilability to add a card without violation to rules and allows Execute function to work 
+	bool Can_Add;  //This is a	flag indecates the avilability to add a card without violation to rules and allows Execute function to work 
 	// Note: These parameters should be read in ReadActionParameters()
 	
 public:
@@ -18,7 +18,7 @@ public:
 	virtual void Execute(); // Creates a new Card Object of the specific Card Number
 	                        // and Reads the Parameters of This Card Number (if any)
 	                        // then Sets this Card Object to GameObject Pointer of its Cell
-	
+							// Adds this card to the grid if it doesn't overlap another game object 
 	virtual ~AddCardAction(); // A Virtual Destructor
 };
 
